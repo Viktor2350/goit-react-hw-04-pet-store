a140de018f63604bad27f73ec54d552ba66618c9
Viktor2350 <ryjuk39@gmail.comm>
2019-10-23 12:17:55 +0300

    Updates

---
+++ precache-manifest.0a34785df520275abbda53b2fbcb2250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e92b515f7bae8b6854ed1074abd14f",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "4da836f27c580b6e948c",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c0efe0e4.chunk.css"
+  },
+  {
+    "revision": "d8bde2766b7eefbf4af1",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.dc6cbca6.chunk.js"
+  },
+  {
+    "revision": "4da836f27c580b6e948c",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.f2860f0c.chunk.js"
+  },
+  {
+    "revision": "892779280bf35b89860e",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.711dd639.js"
+  }
+]);
